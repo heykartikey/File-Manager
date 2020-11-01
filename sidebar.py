b8d540783910f21ceba0a18f0275a8b555987f1d
Kartikey Sahu <heykartikey@users.noreply.github.com>
2020-11-01 22:57:35 +0530

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -5,6 +5,7 @@ class Sidebar(Frame):
     __current = None
 
     def __init__(self, master, bg, activebg, folders, **kwargs):
+        kwargs['bg'] = bg
         super().__init__(master, kwargs)
 
         self.grid_columnconfigure(0, weight=1)
@@ -16,15 +17,18 @@ class Sidebar(Frame):
     def __draw_widgets(self, *folders):
         i = 0
         for icon, name in folders:
-            Label(self, text=name, fg='#333', padx=7, anchor='w', image=icon, compound='left')\
+            Label(self, text=name, fg='#333', padx=7, anchor='w', image=icon, compound='left', bg=self.bg)\
                 .grid(sticky='ew', row=i, column=0, ipady=4)
             i += 1
 
+        # Changing bind tags for the child of Sidebar so that event could be
+        # bind easily using bind_class method
         for child in self.winfo_children():
             tags = list(child.bindtags())
             tags[2] = 'SidebarLabel'
             child.bindtags(tuple(tags))
 
+        # Let the first child be active by default
         self.__current = self.winfo_children()[0]
         self.__current.config(bg=self.activebg)
 
@@ -35,10 +39,12 @@ class Sidebar(Frame):
         self.bind_class('SidebarLabel', '<Button-1>', self.__markactive)
 
     def __mouse_leave(self, event):
+        # Do not switch color if we leave Label that has already been marked active
         if not (widget := event.widget) is self.__current:
             widget.config(bg=self.bg)
 
     def __markactive(self, event):
+        # Switch the active state whenever a label is clicked
         if not (widget := event.widget) is self.__current:
             self.__current.config(bg=self.bg)
             self.__current = widget
