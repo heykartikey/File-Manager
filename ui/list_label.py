a2f5dbe55c97ac9265a8ec223666fa7aa9002eda
Kartikey Sahu <heykartikey@users.noreply.github.com>
2020-11-01 15:56:48 +0530

    Update list_label.py

--- ui/list_label.py
+++ ui/list_label.py
@@ -7,11 +7,15 @@ class ListLabel(Frame):
     _icon = {'file': file, 'dir': folder}
     _active_color = '#eee'
 
+    _current = None
+
     def __init__(self, master, type, *texts, **kwargs):
-        super().__init__(master, **kwargs, bg='#fff')
+        kwargs.setdefault('bg', '#fff')
+        super().__init__(master, **kwargs)
+
         self._name = texts[0]
 
-        Label(self, image=self._icon[type], bg=self.cget('bg'))\
+        Label(self, image=ListLabel._icon[type], bg=self.cget('bg'))\
             .pack(side='left')
 
         for txt in texts:
@@ -20,9 +24,13 @@ class ListLabel(Frame):
 
         self.bind(
             '<Enter>',
-            lambda e: self.child_config(bg=self._active_color)
+            lambda e: self.child_config(bg=ListLabel._active_color)
         )
-        self.bind('<Leave>', lambda e: self.child_config(bg=self.cget('bg')))
+        self.bind('<Leave>', self.__mouse_leave)
+
+        if ListLabel._current is None:
+            ListLabel._current = self
+            ListLabel._current.child_config(bg=ListLabel._active_color)
 
         for child in self.winfo_children():
             tags = list(child.bindtags()) or []
@@ -32,3 +40,17 @@ class ListLabel(Frame):
     def child_config(self, **kwargs):
         for child in self.winfo_children():
             child.config(**kwargs)
+
+    def __mouse_leave(self, event):
+        if ListLabel._current == self:
+            return
+
+        self.child_config(bg=self.cget('bg'))
+
+    @staticmethod
+    def change_current(event):
+        if ListLabel._current:
+            ListLabel._current.child_config(bg=ListLabel._current.cget('bg'))
+
+        ListLabel._current = event.widget.master
+        ListLabel._current.child_config(bg=ListLabel._active_color)
