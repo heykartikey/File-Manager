8095388366e73d2b05617af74207cadd917095b4
Kartikey Sahu <heykartikey@users.noreply.github.com>
2020-11-01 23:00:19 +0530

    Update frame_stack.py

--- ui/frame_stack.py
+++ ui/frame_stack.py
@@ -2,13 +2,14 @@ from tkinter import Frame
 
 
 class FrameStack(Frame):
+    ''' A widget that allows raising its child frame to top when needed '''
+
     def __init__(self, master):
         super().__init__(master)
 
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
-        self.lastframe = None
         self.frames = dict()
 
     def push(self, cls, key):
