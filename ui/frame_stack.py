ee5fd8b5e580a48d36047305c607fa9f5525780d
Kartikey Sahu <heykartikey@users.noreply.github.com>
2020-11-07 11:55:01 +0530

    Update frame_stack.py

--- ui/frame_stack.py
+++ ui/frame_stack.py
@@ -13,8 +13,12 @@ class FrameStack(Frame):
         self.frames = dict()
 
     def push(self, cls, key):
-        self.frames[key] = cls(self)
+        # add a new frame with key 'key'
+        self.frames[key] = cls(master=self)
         self.frames[key].grid(row=0, column=0, sticky='nsew')
+
+        # always raise the latest added frame
+        self.raise_frame(key)
 
     def raise_frame(self, key):
         self.frames[key].lift()
