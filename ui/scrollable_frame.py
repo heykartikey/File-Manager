b1d4230a38acfdb875ee60320c7fa7fafb4e158c
Kartikey Sahu <heykartikey@users.noreply.github.com>
2020-11-07 12:12:31 +0530

    Update scrollable_frame.py

--- ui/scrollable_frame.py
+++ ui/scrollable_frame.py
@@ -5,28 +5,38 @@ class ScrollableFrame(Frame):
     '''Scrollable Frame Widget'''
 
     def __init__(self, master, **kwargs):
-        super().__init__(master, kwargs, bg='#fff')
-        self.tkraise()
+        kwargs.setdefault('bg', 'white')
+
+        super().__init__(master, **kwargs)
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
-        self._canvas = Canvas(self, bg=self.cget('bg'))
-        self._canvas.grid(row=0, column=0, sticky='nsew')
-        self._canvas.columnconfigure(0, weight=1)
+        vscrollbar = Scrollbar(self, orient='vertical')
+        vscrollbar.grid(row=0, column=1, sticky='ns')
 
-        self.__sbar = Scrollbar(self, orient='vertical',
-                                command=self._canvas.yview)
-        self.__sbar.grid(row=0, column=1, sticky='ns')
+        canvas = Canvas(self, bd=0, highlightthickness=0, bg=kwargs['bg'])
+        canvas.grid(row=0, column=0, sticky='nsew')
 
-        self.window = Frame(self._canvas, bg='#fff')
+        vscrollbar.config(command=canvas.yview)
+        canvas.config(yscrollcommand=vscrollbar.set)
+
+        self.window = w = Frame(canvas)
         self.window.columnconfigure(0, weight=1)
 
-        self._canvas.create_window((0, 0), window=self.window,
-                                   anchor='nw', tags='inner')
-        self._canvas.configure(yscrollcommand=self.__sbar.set)
+        inner = canvas.create_window((0, 0), window=w, anchor='nw')
 
-        self.winfo_toplevel().bind("<Configure>", self.__resize)
+        def configure_window(event):
+            r_width, r_height = w.winfo_reqwidth(), w.winfo_reqheight()
+            canvas.config(scrollregion=f'0 0 {r_width} {r_height}')
 
-    def __resize(self, event):
-        self._canvas.configure(scrollregion=self._canvas.bbox("all"))
-        self._canvas.itemconfig('inner', width=self._canvas.winfo_width())
+            if r_width != canvas.winfo_width():
+                canvas.config(width=r_width)
+
+        self.window.bind('<Configure>', configure_window)
+
+        def configure_canvas(event):
+            if w.winfo_reqwidth() != canvas.winfo_width():
+                canvas.itemconfigure(inner, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', configure_canvas)
